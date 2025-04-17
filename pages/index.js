91998319eb66f623ef3c1ffe0d4fb77a543c9025
Arthur Fromont <arthur@MacBook-Pro-de-Arthur.local>
2025-04-17 16:41:01 +0700

    Une petite mise à jour

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-black min-h-screen flex items-center justify-center">
-      <Image
-        src="/logo-sonarmo-officiel.png"
-        alt="Sonarmo logo"
-        width={1280}
-        height={720}
-        priority
-      />
+      <div className="animate-fade-in-up hover:scale-105 hover:rotate-1 transition-all duration-700 ease-in-out">
+        <Image
+          src="/logo-sonarmo-officiel.png"
+          alt="Sonarmo logo"
+          width={1200}
+          height={630}
+          priority
+        />
+      </div>
     </main>
   );
 }
