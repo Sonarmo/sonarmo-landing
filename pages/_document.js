80de97175ba411f8b1d42577646eb6bf5cf50a1f
Arthur Fromont <arthur@MacBook-Pro-de-Arthur.local>
2025-04-17 17:55:00 +0700

    Une petite mise à jour

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        {/* Tu peux aussi ajouter une meta pour le mobile ici plus tard */}
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
