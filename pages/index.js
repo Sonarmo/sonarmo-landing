bfd0dab98a1cb3e0861d46155a6f64b07b5fb17f
Arthur Fromont <arthur@MacBook-Pro-de-Arthur.local>
2025-04-17 16:28:59 +0700

    Une petite mise à jour

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Bienvenue sur Sonarmo la machine</h1>
+      <h1 className="text-4xl font-bold">Bienvenue sur Sonarmo</h1>
     </main>
   );
 }
