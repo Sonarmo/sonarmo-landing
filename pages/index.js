948747ba43e7185abfabebd3ca3d7c1280025a84
Arthur Fromont <arthur@MacBook-Pro-de-Arthur.local>
2025-04-17 18:58:17 +0700

    Une mise à jour

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-black min-h-screen flex items-center justify-center px-4">
-      <div className="w-full max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-3xl">
+      <div className="w-full max-w-[600px] h-auto">
         <Image
           src="/hero-sonarmo.png"
           alt="Teaser Sonarmo"
-          width={1600}
-          height={900}
+          width={600}
+          height={600}
           priority
           className="w-full h-auto object-contain"
         />
@@ -16,4 +16,3 @@ export default function Home() {
     </main>
   );
 }
-
