ab41d3997de57723ad66f7c361d590bfb2707c91
Arthur Fromont <fromont.arthur@gmail.com>
2025-06-02 10:19:16 +0700

    maj white

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,33 @@
 import "/styles/globals.css";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import DashboardLayout from "/components/builder/DashboardLayout"; // chemin relatif
+import DashboardLayout from "/components/builder/DashboardLayout";
 import { PlayerProvider } from "/lib/contexts/PlayerContext";
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isDashboard = router.pathname.startsWith("/dashboard");
 
   const getLayout = isDashboard
-    ? (page) => <DashboardLayout>{page}</DashboardLayout>
+    ? (page) => (
+        <PlayerProvider>
+          <DashboardLayout>{page}</DashboardLayout>
+        </PlayerProvider>
+      )
     : (page) => page;
 
   return getLayout(
-  <PlayerProvider>
-    <Head>
-      <title>Sonarmo</title>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <Component {...pageProps} />
-  </PlayerProvider>
-);
+    <>
+      <Head>
+        <title>Sonarmo</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
