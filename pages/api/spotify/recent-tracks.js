6d0e257fa846ce1688c91913d5ac3f881d984baf
Arthur Fromont <fromont.arthur@gmail.com>
2025-06-02 09:05:12 +0700

    maj file d'attente3

--- pages/api/spotify/recent-tracks.js
+++ pages/api/spotify/recent-tracks.js
@@ -1,5 +1,5 @@
 // /pages/api/spotify/recent-tracks.js
-import { getTokenFromFirebase } from '/lib/auth';
+import { getTokenFromFirebase } from 'lib/auth';
 
 export default async function handler(req, res) {
   try {
