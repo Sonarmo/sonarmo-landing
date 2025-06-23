6452b6206ccc6697c1303b689cb723482fdd5395
Arthur Fromont <fromont.arthur@gmail.com>
2025-06-23 13:25:42 +0200

    analytics12

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@
 import "/styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import DashboardLayout from "/components/builder/DashboardLayout";
 import { PlayerProvider } from "/lib/contexts/PlayerContext";
 
@@ -11,11 +11,11 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isDashboard = router.pathname.startsWith("/dashboard");
 
-  // Suivi des pages (si GA est activé)
+  // Suivi des pages (si GA actif via GTM)
   useEffect(() => {
     const handleRouteChange = (url) => {
       if (typeof window.gtag !== "undefined") {
-        window.gtag("config", "G-PTGDLQ7W2N", {
+        window.gtag("event", "page_view", {
           page_path: url,
         });
       }
@@ -30,10 +30,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Sonarmo</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/sonarmo-experience.png" type="image/png" />
       </Head>
 
@@ -43,29 +39,6 @@ function MyApp({ Component, pageProps }) {
         strategy="afterInteractive"
         src="https://cdn-cookieyes.com/client_data/c09dfc653764ff663ca49778/script.js"
       />
-
-      {/* ✅ Google Analytics déclenché via CookieYes */}
-      <Script id="cookieyes-ga-handler" strategy="afterInteractive">
-        {`
-          window.cookieyesCallbacks = window.cookieyesCallbacks || [];
-          window.cookieyesCallbacks.push(function() {
-            if (CookieYes.consent.analytics) {
-              const gtagScript = document.createElement('script');
-              gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-PTGDLQ7W2N";
-              gtagScript.async = true;
-              document.head.appendChild(gtagScript);
-
-              gtagScript.onload = function () {
-                window.dataLayer = window.dataLayer || [];
-                function gtag() { dataLayer.push(arguments); }
-                window.gtag = gtag;
-                gtag('js', new Date());
-                gtag('config', 'G-PTGDLQ7W2N', { anonymize_ip: true });
-              };
-            }
-          });
-        `}
-      </Script>
 
       <Component {...pageProps} />
     </>
--- pages/_documents.js
+++ pages/_documents.js
@@ -1,18 +1,42 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K8ZSPSKK');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8ZSPSKK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
