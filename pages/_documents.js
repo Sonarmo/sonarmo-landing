edd2a37d02aba57d521f50033256e90397f76f70
Arthur Fromont <fromont.arthur@gmail.com>
2025-06-23 14:55:03 +0200

    analytics20

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,17 +1,39 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* ✅ Font Google (sans warning) */}
+        {/* ✅ Font Google */}
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+
+        {/* ✅ Google Tag Manager script (head) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K8ZSPSKK');
+            `,
+          }}
+        />
       </Head>
       <body>
+        {/* ✅ Google Tag Manager noscript (body top) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8ZSPSKK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
