73cbd5c2941477d3c2ade131e1459e598d0aeb0b
Arthur Fromont <fromont.arthur@gmail.com>
2025-06-23 14:09:40 +0200

    analytics18

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isDashboard = router.pathname.startsWith("/dashboard");
 
-  // Suivi page view GA (sera activé automatiquement si le consentement est donné)
+  // GA : suivi page view
   useEffect(() => {
     const handleRouteChange = (url) => {
       if (typeof window.gtag !== "undefined") {
@@ -30,41 +30,40 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Sonarmo</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/sonarmo-experience.png" type="image/png" />
-
-        {/* ✅ Cookiebot */}
-        <script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="6b424237-bb4c-47c8-8c50-959161d7da5e"
-          data-blockingmode="auto"
-          type="text/javascript"
-        ></script>
-
-        {/* ✅ Google Analytics (sera bloqué tant que le consentement n'est pas donné) */}
-        <script
-          type="text/plain"
-          data-cookieconsent="statistics"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-PTGDLQ7W2N"
-        ></script>
-        <script
-          type="text/plain"
-          data-cookieconsent="statistics"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-PTGDLQ7W2N', { anonymize_ip: true });
-            `,
-          }}
-        />
       </Head>
+
+      {/* ✅ Cookiebot */}
+      <Script
+        id="Cookiebot"
+        strategy="beforeInteractive"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="6b424237-bb4c-47c8-8c50-959161d7da5e"
+        data-blockingmode="auto"
+        type="text/javascript"
+      />
+
+      {/* ✅ Google Analytics via Cookiebot */}
+      <Script
+        id="gtag-script"
+        type="text/plain"
+        data-cookieconsent="statistics"
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-PTGDLQ7W2N"
+      />
+      <Script
+        id="gtag-init"
+        type="text/plain"
+        data-cookieconsent="statistics"
+        strategy="lazyOnload"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PTGDLQ7W2N', { anonymize_ip: true });
+        `}
+      </Script>
 
       <Component {...pageProps} />
     </>
