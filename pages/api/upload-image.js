8a42cf53dc5c6fa1b5fdba750cdbe377aa714cd8
Arthur Fromont <fromont.arthur@gmail.com>
2025-07-08 12:36:57 +0200

    debug image 9

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { file: image, fileName } = req.body;
+    const { image, fileName } = req.body;
 
     if (!image || !fileName) {
       return res.status(400).json({ error: "Image ou nom de fichier manquant" });
